8cd783851d3f4cdbdc3605d4262a2bd4bc7f330c
pedrohmenezes1 <pedro.mk.13@hotmail.com>
2022-01-29 09:31:14 -0300

    feat: add validation and pick

---
+++ src/utils/pick.js
@@ -0,0 +1,17 @@
+/**
+ * Cria um objeto composto pelas propriedades do objeto selecionado
+ * @param {Object} object
+ * @param {string[]} keys
+ * @returns {Object}
+ */
+const pick = (object, keys) => {
+  return keys.reduce((obj, key) => {
+    if (object && Object.prototype.hasOwnProperty.call(object, key)) {
+      // eslint-disable-next-line no-param-reassign
+      obj[key] = object[key];
+    }
+    return obj;
+  }, {});
+};
+
+module.exports = pick;
