10937b33ccda22cd6f41ae848f119583a2ae16bd
pedrohmenezes1 <pedro.mk.13@hotmail.com>
2022-01-30 11:52:40 -0300

    feat:authenticate files and fix

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,7 +1,7 @@
 const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const config = require('./config');
 const { tokenTypes } = require('./tokens');
-const { User } = require('../models');
+const { People } = require('../models');
 
 const jwtOptions = {
   secretOrKey: config.jwt.secret,
@@ -13,11 +13,11 @@ const jwtVerify = async (payload, done) => {
     if (payload.type !== tokenTypes.ACCESS) {
       throw new Error('Invalid token type');
     }
-    const user = await User.findById(payload.sub);
-    if (!user) {
+    const people = await People.findById(payload.sub);
+    if (!people) {
       return done(null, false);
     }
-    done(null, user);
+    done(null, people);
   } catch (error) {
     done(error, false);
   }
