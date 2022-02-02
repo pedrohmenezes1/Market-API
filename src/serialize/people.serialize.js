a7d16a404aa942c9ecaf1219413564ff3ca26f4b
pedrohmenezes1 <pedro.mk.13@hotmail.com>
2022-02-02 20:57:34 -0300

    feat:people serialize

---
+++ src/serialize/people.serialize.js
@@ -0,0 +1,21 @@
+const moment = require('moment');
+
+// eslint-disable-next-line camelcase
+const serialize = ({ _id, nome, cpf, data_nascimento, email, habilitado }) => ({
+  _id,
+  nome,
+  cpf,
+  data_nascimento: moment(data_nascimento).utc().format('DD/MM/YYYY'),
+  email,
+  habilitado,
+});
+
+const paginateSerialize = ({ docs, limit, totalDocs, pagingCounter, totalPages }) => ({
+  pessoas: docs.map(serialize),
+  limit,
+  total: totalDocs,
+  offset: pagingCounter,
+  offsets: totalPages,
+});
+
+module.exports = { serialize, paginateSerialize };
