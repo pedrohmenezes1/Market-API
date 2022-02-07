269ebc155b821b98d0f1997d496c6334cf69487d
pedrohmenezes1 <pedro.mk.13@hotmail.com>
2022-02-06 22:10:47 -0300

    feat:Post car swagger

--- src/routes/v1/cars.route.js
+++ src/routes/v1/cars.route.js
@@ -17,3 +17,65 @@ router
   .patch(auth, validate(carsValidation.updateCars), carsController.updateCars)
   .get(auth, validate(carsValidation.getCars), carsController.getCarsId);
 module.exports = router;
+
+/**
+ * @swagger
+ * tags:
+ *   name: Cars
+ *   description: CRUD de carros
+ */
+
+/**
+ * @swagger
+ * /car:
+ *   post:
+ *      summary: Cadastrar carros
+ *      description: Só pode cadastrar carros se estiver logado.
+ *      tags: [Cars]
+ *      security:
+ *        - bearerAuth: []
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              required:
+ *                - modelo
+ *                - cor
+ *                - ano
+ *                - acessorios
+ *                - quantidadedePassageiros
+ *              properties:
+ *                modelo:
+ *                  type: string
+ *                cor:
+ *                  type: string
+ *                ano:
+ *                  type: string
+ *                acessorios:
+ *                  type: array
+ *                  description: tem que ter no mínimo 1
+ *                quantidadedePassageiros:
+ *                  type: string
+ *              example:
+ *                modelo: GM S10 2.8
+ *                cor: Branco
+ *                ano: 2019
+ *                acessorios: [
+ *                      descrição: Ar-condicionado,
+ *                      descrição: 4 Portas]
+ *      responses:
+ *        "201":
+ *          description: Created
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/Car'
+ *        "400":
+ *          $ref: '#/components/responses/ValidationError'
+ *        "401":
+ *          $ref: '#/components/responses/Unauthorized'
+ *        "500":
+ *          $ref: '#/components/responses/RequestError'
+ */
