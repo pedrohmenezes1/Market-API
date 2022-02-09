3a08b2eba2d777420ec9cb9eb0e7d3afaa51480a
pedrohmenezes1 <pedro.mk.13@hotmail.com>
2022-02-09 20:07:39 -0300

    feat:tests people.fixtures

---
+++ tests/fixtures/people.fixture.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const faker = require('faker');
+const People = require('../../src/models/people.model');
+
+const senha = 'senha1';
+const salt = bcrypt.genSaltSync(8);
+const hashedPassword = bcrypt.hashSync(senha, salt);
+
+const peopleOne = {
+  _id: mongoose.Types.ObjectId(),
+  name: faker.name.findName(),
+  cpf: '118.184.154-56',
+  data_nascimento: '23/07/1998',
+  email: faker.internet.email().toLowerCase(),
+  senha,
+  habilitado: 'Sim',
+};
+
+const peopleTwo = {
+  _id: mongoose.Types.ObjectId(),
+  name: faker.name.findName(),
+  cpf: '118.184.154-56',
+  data_nascimento: '23/07/1998',
+  email: faker.internet.email().toLowerCase(),
+  senha,
+  habilitado: 'Sim',
+};
+const insertPeoples = async (peoples) => {
+  await People.insertMany(peoples.map((people) => ({ ...people, senha: hashedPassword })));
+};
+
+module.exports = {
+  peopleOne,
+  peopleTwo,
+  insertPeoples,
+};
