d52023935e49454976ecfb481dd6e6896431c8be
pedrohmenezes1 <pedro.mk.13@hotmail.com>
2022-02-19 14:46:15 -0300

    feat: controller list rental

--- src/controllers/rental.controller.js
+++ src/controllers/rental.controller.js
@@ -1,5 +1,6 @@
 const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
+const pick = require('../utils/pick');
 const { rentalService } = require('../services');
 const { serialize } = require('../serialize/people.serialize');
 
@@ -8,6 +9,25 @@ const createRental = catchAsync(async (req, res) => {
   res.status(httpStatus.CREATED).send(serialize(result));
 });
 
+const getRental = catchAsync(async (req, res) => {
+  const filter = pick(req.query, [
+    'nome',
+    'cnpj',
+    'atividades',
+    'endereco.cep',
+    'endereco.logradouro',
+    'endereco.complemento',
+    'endereco.bairro',
+    'endereco.number',
+    'endereco.localidade',
+    'endereco.uf',
+  ]);
+  const options = pick(req.query, ['limit', 'offset']);
+  const result = await rentalService.rentalList(filter, options);
+  res.status(200).send(result);
+});
+
 module.exports = {
   createRental,
+  getRental,
 };
