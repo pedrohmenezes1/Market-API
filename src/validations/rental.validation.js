42b5f97ed63fd88318a2f62ecb220e802614d4f1
pedrohmenezes1 <pedro.mk.13@hotmail.com>
2022-02-19 15:29:55 -0300

    feat: rental validations

--- src/validations/rental.validation.js
+++ src/validations/rental.validation.js
@@ -39,8 +39,28 @@ const getRentalById = {
     rentalId: Joi.required().custom(objectId),
   }),
 };
+
+const updateRental = {
+  nome: Joi.string().trim(),
+  cnpj: Joi.string().regex(/^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/),
+  atividades: Joi.string().trim(),
+  endereco: Joi.array()
+    .unique()
+    .min(1)
+    .items({
+      cep: Joi.string()
+        .regex(/^\d{5}-\d{3}$/)
+        .trim()
+        .required(),
+      complemento: Joi.string().trim().optional(),
+      number: Joi.number().required(),
+      isFilial: Joi.boolean().required(),
+    }),
+};
+
 module.exports = {
   createRental,
   getRental,
   getRentalById,
+  updateRental,
 };
