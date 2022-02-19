6faf50a6c73229dc4944e9c615eb2b5d17d4db04
pedrohmenezes1 <pedro.mk.13@hotmail.com>
2022-02-19 14:07:06 -0300

    feat:rental router post

--- src/routes/v1/rental.route.js
+++ src/routes/v1/rental.route.js
@@ -1,6 +1,9 @@
-/* const express = require('express');
+const express = require('express');
 const auth = require('../../middlewares/auth');
 const validate = require('../../middlewares/validate');
 const rentalValidation = require('../../validations/rental.validation');
 const rentalController = require('../../controllers/rental.controller');
- */
+
+const router = express.Router();
+
+router.route('/').post(auth, validate(rentalValidation.createRental), rentalController.createRental);
