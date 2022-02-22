4f7cd86d4ef728eeff9fc18546152a51105a71d7
pedrohmenezes1 <pedro.mk.13@hotmail.com>
2022-02-22 20:52:38 -0300

    feat:crud:fleet

--- src/services/fleet.service.js
+++ src/services/fleet.service.js
@@ -15,6 +15,55 @@ const createFleet = async (rentalId, fleetBody) => {
   return fleetRepository.createFleet(rentalId, fleetBody);
 };
 
+/**
+ * Listar frotas
+ * @param {Object} filter - Mongo filter
+ * @param {Object} options - Query options
+ * @param {number} [options.limit] - Número máximo de resultados por página (padrão = 100)
+ * @param {number} [options.offset] - Página atual (padrão = 1)
+ * @returns {Promise<QueryResult>}
+ */
+const fleetList = async (filter, options) => {
+  const fleetResult = await fleetRepository.findFleet(filter, options);
+  return fleetResult;
+};
+
+/**
+ * Deletar frotas por id
+ * @param {ObjectId} fleetId
+ * @returns {Promise<fleetRepository>}
+ */
+const deleteFleetById = async (fleetId) => {
+  const fleetResult = await fleetRepository.getFleetId(fleetId);
+  if (!fleetResult) {
+    throw new MarketError(httpStatus.NOT_FOUND, 'Frota de veículos não encontrada');
+  }
+  await fleetResult.remove();
+  return fleetResult;
+};
+
+/**
+ * Cadastrar uma nova frota
+ * @param {ObjectId} fleetId
+ * @param {Object} updateBody
+ * @returns {Promise<fleetRepository>}
+ */
+const updateFleetById = async (fleetId, updateBody) => {
+  const fleetResult = await fleetRepository.getFleetId(fleetId);
+  if (!fleetResult) {
+    throw new MarketError(httpStatus.NOT_FOUND, 'Frota de veículos não encontrado');
+  }
+  if (await fleetRepository.isPlateTaken(fleetRepository.placa)) {
+    throw new MarketError(httpStatus.BAD_REQUEST, 'Placa já existente');
+  }
+  Object.assign(fleetResult, updateBody);
+  await fleetResult.save();
+  return fleetResult;
+};
+
 module.exports = {
   createFleet,
+  fleetList,
+  deleteFleetById,
+  updateFleetById,
 };
